a673e46c57630a6f7d8865643d49fb1685355ce1
Fluri Jasmin <jasmin.fluri@fhnw.ch>
2020-05-25 08:50:41 +0200

    procedure updated to start camunda process

--- src/main/db/procedures/START_CAMUNDA_PROCESS.sql
+++ src/main/db/procedures/START_CAMUNDA_PROCESS.sql
@@ -1,4 +1,4 @@
-create or replace procedure start_camunda_process is
+create or replace procedure start_camunda_process(proposalId IN number) is
 
   l_body clob := '';
   l_clob clob := '';
@@ -25,5 +25,19 @@ begin
               p_body => l_body
   );
   print_clob_to_output (l_clob); 
+  
+  
+  -- {"links":[{"method":"GET","href":"https://emmentaler.herokuapp.com/rest/process-instance/23c47805-9e4d-11ea-9fe5-3e17a72f504f","rel":"self"}],"id":"23c47805-9e4d-11ea-9fe5-3e17a72f504f","definitionId":"digibp-tobe:2:dfaedcba-9de2-11ea-9c51-da029e11a7df","
+  -- businessKey":"proposalProcessAPEX","caseInstanceId":null,"ended":false,"suspended":false,"tenantId":null}
+
+  
+
+  update antrag set instance_information = l_clob where id = proposalId;
+  commit;
+  
+  update antrag set   camunda_instance = json_value(instance_information, '$.id' returning VARCHAR2) where id = proposalId;
+  commit;
+  
+   
   
 end start_camunda_process;
